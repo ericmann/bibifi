d9888a0cfad7386f8a21bdb2a971613707bb9ed5
Eric Mann <eric@eamann.com>
2015-05-09 20:43:17 -0700

    CLI parsing for logappend

---
+++ src/logappend.js
@@ -0,0 +1,14 @@
+/**
+ * Secure logging mechanism
+ *
+ * @author Eric Mann <eric@eamann.com>
+ *
+ * MIT License.
+ */
+
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var cli = new (require( './lib/cli' ));
