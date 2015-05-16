03aa8a995ce7eec5bbef53f36badf765740a8a65
Eric Mann <eric@eamann.com>
2015-05-15 20:08:34 -0700

    New log format

--- src/lib/cli.js
+++ src/lib/cli.js
@@ -23,8 +23,135 @@ function CLI() {
 	this.argv = process.argv.slice( 2 );
 }
 
+/**
+ * Parse and somewhat validate the command-line arguments for logappend.
+ *
+ * If we have certain kinds of collisions, the validation will exit immediately, returning the default status of 'invalid':
+ * - Both -E and -G are used
+ * - Both -A and -L are used
+ *
+ * @param {Array} [argv]
+ *
+ * @returns {Object}
+ */
 CLI.prototype.validate_append_args = function( argv ) {
+	if ( undefined === argv ) {
+		// Allow a global array if needed
+		argv = this.argv;
+	}
 
+	var append_query = {
+			'type'        : 'entry',
+			'status'      : 'invalid',
+			'key'         : null,
+			'file'        : null,
+			'time'        : null,
+			'action'      : null,
+			'room'        : null,
+			'name'        : null,
+			'visitor_type': null
+		},
+		type_selected = false,
+		action_selected = false;
+
+	for ( var i = 0, l = argv.length; i < l; i++ ) {
+		var param = argv[i];
+
+		// Grab a key parameter
+		if ( param.indexOf( '-K' ) === 0 ) {
+			// If the key is standalone
+			if ( param === '-K' ) {
+				i += 1;
+				append_query.key = argv[ i ];
+			} else {
+				append_query.key = param.replace( '-K', '' );
+			}
+		}
+		// Grab a time parameter
+		else if ( param.indexOf( '-T' ) === 0 ) {
+			if ( param === '-T' ) {
+				i += 1;
+				append_query.time = argv[ i ];
+			} else {
+				append_query.time = param.replace( '-T', '' );
+			}
+		}
+		// Grab a room parameter
+		else if ( param.indexOf( '-R' ) === 0 ) {
+			if ( param === '-R' ) {
+				i += 1;
+				append_query.room = argv[ i ];
+			} else {
+				append_query.room = param.replace( '-R', '' );
+			}
+		}
+		// Grab an employee name
+		else if ( param.indexOf( '-E' ) === 0 ) {
+			// Only one type is allowed
+			if ( type_selected && type_selected !== 'E' ) {
+				return append_query;
+			}
+
+			if ( param === '-E' ) {
+				i += 1;
+				append_query.name = argv[ i ];
+			} else {
+				append_query.name = param.replace( '-E', '' );
+			}
+			append_query.visitor_type = 'E';
+			type_selected = 'E';
+		}
+		// Grab a guest name
+		else if ( param.indexOf( '-G' ) === 0 ) {
+			// Only one type is allowed
+			if ( type_selected && type_selected !== 'G' ) {
+				return append_query;
+			}
+
+			if ( param === '-G' ) {
+				i += 1;
+				append_query.name = argv[ i ];
+			} else {
+				append_query.name = param.replace( '-G', '' );
+			}
+			append_query.visitor_type = 'G';
+			type_selected = 'G';
+		}
+		// Grab an arrive action
+		else if ( param.indexOf( '-A' ) === 0 ) {
+			// Only one type is allowed
+			if ( action_selected && action_selected !== 'A' ) {
+				return append_query;
+			}
+
+			append_query.action = 'A';
+			action_selected = 'A';
+		}
+		// Grab a leave action
+		else if ( param.indexOf( '-L' ) === 0 ) {
+			// Only one type is allowed
+			if ( action_selected && action_selected !== 'L' ) {
+				return append_query;
+			}
+
+			append_query.action = 'L';
+			action_selected = 'L';
+		}
+		// Are we a batch?
+		else if ( param.indexOf( '-B' ) === 0 ) {
+			append_query.type = 'batch';
+		}
+		// Get the logfile name
+		else {
+			append_query.file = param;
+		}
+	}
+
+	// If we're good, we're valid
+	append_query['status'] = 'valid';
+
+	// Return our processed entry
+	return append_query;
 };
 
 /**
--- src/logappend.js
+++ src/logappend.js
@@ -14,126 +14,25 @@
 var _ = require( 'lodash' ),
 	fs = require( 'fs' ),
 	cli = new (require( './lib/cli' )),
+	LogFile = require( './lib/logfile' ),
 	util = require( './lib/util' ),
-	Entry = require( './lib/entry' ),
-	Logger = require( './lib/logger' );
+	Entry = require( './lib/entry' );
 
 var argv = process.argv.slice( 2 ),
 	logs = {};
 
-/**
- * Append an entry to a logfile (specified by the entry)
- *
- * @param {Entry}   entry
- * @param {String}  logfile
- * @param {String}  secret
- * @param {Boolean} [handleError]
- */
-function appendEntry( entry, logfile, secret, handleError ) {
-	if ( undefined === handleError ) {
-		handleError = true;
-	}
+// Validate the entry arguments
+var append = cli.validate_append_args();
 
-	if ( undefined === logs[ logfile ] ) {
+// Get a log file
+var log = new LogFile( append.file, append.key );
 
-		var log = Logger.prototype.open( logfile, secret );
+console.log( log.metaPointer() );
 
-		// The Logger utility returns error messages when needed
-		if ( 'key_err' === log ) {
-			process.stdout.write( 'invalid' );
-			if ( handleError ) {
-				process.exit( 255 );
-			}
-			return; // Continue to the next entry
-		}
+console.log( log );
 
-		logs[ logfile ] = log;
-	}
-
-	logs[ logfile ].append( entry, handleError );
-}
-
-// Determine if we have a batchfile or not
-if ( _.find( argv, function( item ) { return item.indexOf( '-B' ) == 0; } ) ) {
-	var batch, file, i, l;
-
-	// Get the batchfile name
-	for ( i = 0, l = argv.length; i < l; i++ ) {
-		if ( argv[ i ].indexOf( '-B' ) === 0 ) {
-			continue;
-		}
-
-		batch = argv[ i ];
-		break;
-	}
-
-	// Load the batchfile
-	try {
-		file = fs.readFileSync( batch );
-		file = file.toString();
-	} catch ( e ) {
-		return util.invalid();
-	}
-
-	var lines = file.split( '\n' );
-
-	// Get an entry for each line in the batchfile
-	var entries = [];
-
-	// Validate each entry in the batchfile
-	for ( i = 0, l = lines.length; i < l; i ++ ) {
-		var line = lines[ i ];
-
-		if ( '' === line.trim() ) {
-			continue;
-		}
-
-		var line_argv = line.split( /\s/ ),
-			line_entry = cli.validate_entry( line_argv );
-
-		if ( 'invalid' === line_entry.status ) {
-			process.stdout.write( 'invalid' );
-		} else {
-			entries.push( line_entry );
-		}
-	}
-
-	// Append each entry
-	_.forEach( entries, function( entry ) {
-		// Convert and double-check our entry
-		var sanitized = new Entry( entry.data );
-
-		if ( ! sanitized.isValid() ) {
-			process.stdout.write( 'invalid' );
-			return; // Move to the next item
-		}
-
-		appendEntry( sanitized, entry.data.logfile, entry.data.secret, false );
-	} );
-} else {
-	// Validate the entry
-	var entry = cli.validate_entry();
-
-	// If we're invalid, exit
-	if ( 'invalid' === entry.status ) {
-		return util.invalid();
-	}
-
-	// Convert and double-check our entry
-	var sanitized = new Entry( entry.data );
-
-	if ( ! sanitized.isValid() ) {
-		return util.invalid();
-	}
-
-	// Append the entry
-	appendEntry( sanitized, entry.data.logfile, entry.data.secret );
-}
-
-// Now, close out all of the logs
-_.forEach( logs, function( log ) {
-	log.write();
-} );
+// We're done, so let's close the logfile
+log.close();
 
 // Fin
 process.exit( 0 );
